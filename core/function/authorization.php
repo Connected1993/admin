4a13cb6c6e64c78fe62d708182c57720b86f9096
Alexander <alexanderbalabas1993@gmail.com>
2023-09-03 12:21:07 +0300

    add authorization

--- core/function/authorization.php
+++ core/function/authorization.php
@@ -3,12 +3,25 @@
 $account =  (isset($_POST['account']))? $_POST['account'] : null;
 $password = (isset($_POST['password']))? $_POST['password'] : null;
 
-$sql = " SELECT * FROM users WHERE login = '$account' AND password = '$password' ";
-echo $sql;
-// отправили селект в базу
-// получили результат в виде массива
-$result = $dbh->query($sql)->fetch();
 
+$sql = " SELECT * FROM users WHERE ( login = :param1 or email =:param1 or phone =:param1 ) AND password = :param2 ";
+
+// подготовить запрос
+$stm = $dbh->prepare($sql);
+// отправить
+$stm->execute(['param1'=>$account,'param2'=>$password]);
+
+$result = $stm->fetch();
+
+if ($result)
+{
+    echo 'Вошли';
+}
+else
+{
+    echo 'Неверный логин или пароль';
+}
+// получили результат в виде массива
 debug($result);
 
 //( login = '$account' or email = '$account' or phone = '$account' )
